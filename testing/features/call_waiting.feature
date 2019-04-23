622da2cff69ef89f11d36da5465fd81194c3cca4
ngeiger2 <46730638+ngeiger2@users.noreply.github.com>
2019-04-23 11:02:48 -0400

    Created first test

---
+++ testing/features/call_waiting.feature
@@ -0,0 +1,7 @@
+Feature: sign up to Call Waiting
+
+	Scenario: Go to customer registration page
+		Given we are at the homepage
+		When we click on Customer Login
+		And we click on Create an Account
+		Then we should go to the customer registration page
---
+++ testing/features/steps/steps.py
@@ -0,0 +1,23 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+@given(u'we are at the homepage')
+def step_impl(context):
+	context.browser = webdriver.Firefox() #Open browser
+	context.browser.get('http://callwaiting.pythonanywhere.com') #Go to site
+
+@when(u'we click on Customer Login')
+def step_impl(context):
+	elem = context.browser.find_element_by_class_name('btn') #Find login button (do this twice to go to provider's)
+	elem.click() #Click to open login menu
+
+@when(u'we click on Create an Account')
+def step_impl(context):
+	elem = context.browser.find_element_by_link_text('Create an Account') #Find create account button
+	elem.click() #Click to go to create account page
+
+@then(u'we should go to the customer registration page')
+def step_impl(context):
+	assert "http://callwaiting.pythonanywhere.com/register" in context.browser.title
