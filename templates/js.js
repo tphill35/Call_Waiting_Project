515c3494af16c1e32b3fedbd68eb4aa3d47f728c
tphill35 <46730543+tphill35@users.noreply.github.com>
2019-04-25 16:01:22 -0400

    additional styling, temporary jinja additions

--- templates/js.js
+++ templates/js.js
@@ -2,3 +2,13 @@ var typed = new Typed('.element', {
   strings: ["First sentence.", "Second sentence."],
   typeSpeed: 30
 });
+
+$(document).ready(function(){
+    $("#AddFormButton").click(function(){
+        $("#AddForm").toggle();
+    });
+	$("#RemoveFormButton").click(function(){
+        $("#RemoveForm").toggle();
+    });
+});
+
