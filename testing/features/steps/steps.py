ab23e14eaa3de7440796c57a4c5c4de699a25075
ngeiger2 <46730638+ngeiger2@users.noreply.github.com>
2019-04-26 05:03:40 -0400

    Added chromedriver & setup guide
    - Added ability to use chromedriver
    - Added BDD setup guide
    - For some reason the url assert is failing, so now it checks the source for text instead

--- testing/features/steps/steps.py
+++ testing/features/steps/steps.py
@@ -5,7 +5,11 @@ import time
 
 @given(u'we are at the homepage')
 def step_impl(context):
-	context.browser = webdriver.Firefox() #Open browser
+	#Open browser
+	try:
+		context.browser = webdriver.Chrome()
+	except:
+		context.browser = webdriver.Firefox()
 	context.browser.get('http://callwaiting.pythonanywhere.com') #Go to site
 
 @when(u'we click on Customer Login')
@@ -20,4 +24,5 @@ def step_impl(context):
 
 @then(u'we should go to the customer registration page')
 def step_impl(context):
-	assert "http://callwaiting.pythonanywhere.com/register" in context.browser.title
+        assert "Please fill in this form to create an account." in context.browser.page_source
+        #assert "http://callwaiting.pythonanywhere.com/register" in context.browser.title
