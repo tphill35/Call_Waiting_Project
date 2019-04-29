183148a7ff71c84c0b196ac330cdf6ea30e4b67f
ngeiger2 <46730638+ngeiger2@users.noreply.github.com>
2019-04-29 03:29:16 -0400

    Modified setup, added sign up stub
    -Browser is now only opened once per test run
    -Random user info is now generated
    -Sign up test will fill in fields but doesn't submit yet

--- testing/features/call_waiting.feature
+++ testing/features/call_waiting.feature
@@ -5,3 +5,9 @@ Feature: sign up to Call Waiting
 		When we click on Customer Login
 		And we click on Create an Account
 		Then we should go to the customer registration page
+
+	Scenario: Sign up as a caller
+		Given we are at the sign up page
+		When we fill in all fields
+		And we click submit
+		Then we should be able to login
--- testing/features/steps/steps.py
+++ testing/features/steps/steps.py
@@ -1,21 +1,36 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+import random
+import string
+
+#Random string generator
+def randomString(stringLength):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+#Open browser
+try:
+	browser = webdriver.Chrome()
+except:
+	browser = webdriver.Firefox()	
+
+#Generate user info
+firstName = randomString(6)
+lastName = randomString(6)
+password = randomString(8)
+
+#Scenario: navigate to sign up page
 
 @given(u'we are at the homepage')
 def step_impl(context):
-	#Open browser
-	try:
-		context.browser = webdriver.Chrome()
-	except:
-		context.browser = webdriver.Firefox()
-	context.browser.get('http://callwaiting.pythonanywhere.com') #Go to site
+	context.browser = browser #Set browser
+	context.browser.get('http://callwaiting.pythonanywhere.com') #Go to home
 
 @when(u'we click on Customer Login')
 def step_impl(context):
-	elem = context.browser.find_element_by_class_name('btn') #Find login button (do this twice to go to provider's)
-	elem.click() #Click to open login menu
+        elem = context.browser.find_elements_by_class_name('btn')[0] #Find first "btn" class
+        elem.click()
 
 @when(u'we click on Create an Account')
 def step_impl(context):
@@ -24,5 +39,42 @@ def step_impl(context):
 
 @then(u'we should go to the customer registration page')
 def step_impl(context):
-        assert "Please fill in this form to create an account." in context.browser.page_source
-        #assert "http://callwaiting.pythonanywhere.com/register" in context.browser.title
+        assert 'Please fill in this form to create an account.' in context.browser.page_source
+        #assert 'http://callwaiting.pythonanywhere.com/register' in context.browser.title
+
+#Scenario: sign up as caller (stub)
+
+@given(u'we are at the sign up page')
+def step_impl(context):
+        context.browser = browser #Set browser
+        context.browser.get('http://callwaiting.pythonanywhere.com/register') #Go to sign up
+
+@when(u'we fill in all fields')
+def step_impl(context):
+        #First name
+        elem = context.browser.find_element_by_name('firstname')
+        elem.send_keys(firstName)
+        #Last name
+        elem = context.browser.find_element_by_name('lastname')
+        elem.send_keys(lastName)
+        #Email
+        elem = context.browser.find_element_by_name('email')
+        elem.send_keys('veryhomo@cock.li')
+        #Phone
+        elem = context.browser.find_element_by_name('phonenumber')
+        elem.send_keys('222-222-2222')
+        #Password
+        elem = context.browser.find_element_by_name('psw')
+        elem.send_keys('benis789')
+        #Confirm pw
+        elem = context.browser.find_element_by_name('psw-repeat')
+        elem.send_keys('benis789')
+
+@when(u'we click submit')
+def step_impl(context):
+        assert 1
+
+@then(u'we should be able to login')
+def step_impl(context):
+        assert 1
+
